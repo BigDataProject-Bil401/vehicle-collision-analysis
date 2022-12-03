759b2d8a9f3405a27a2d4288e3b848082d69fba8
ibahadiraltun <ibahadiraltun@gmail.com>
2022-12-03 14:39:59 +0300

    Implement coordinate clustering based on partition of collision dataset

--- src/main/scala/Clusterer.scala
+++ src/main/scala/Clusterer.scala
@@ -1,7 +1,11 @@
 import org.apache.spark.SparkContext
 import org.apache.spark.SparkConf
-
+import org.apache.spark.mllib.clustering.KMeans
+import org.apache.spark.mllib.linalg.Vectors
 import Constants.FINAL_DATA_PATH
+import Constants.PARTITION_RATIO
+import Constants.NUM_CLUSTERS
+import Constants.NUM_ITERATIONS
 
 object Clusterer {
   def main(args: Array[String]): Unit = {
@@ -17,6 +21,30 @@ object Clusterer {
       .option("delimiter", ",")
       .csv(FINAL_DATA_PATH)
 
-    base_df.show(5)
+    val null_filtered_df = base_df.filter(
+      base_df("LATITUDE").isNotNull && base_df("LONGITUDE").isNotNull
+    )
+    val final_filtered_df = null_filtered_df.filter(
+      row => row.get(4).toString.toDouble != 0 && row.get(5).toString.toDouble != 0
+    )
+
+    println(final_filtered_df.show(5))
+    println("df count -> ", final_filtered_df.count(),
+      " ---- df partition count -> ", final_filtered_df.count() * PARTITION_RATIO)
+
+    val partitionRDD = final_filtered_df.rdd
+      .sample(withReplacement = false, fraction = PARTITION_RATIO)
+      .cache()
+
+    val trainRDD = partitionRDD.map(
+      s => Vectors.dense(s.get(4).toString.toDouble, s.get(5).toString.toDouble)
+    )
+
+    println("train starts -> ")
+    val clusters = KMeans.train(trainRDD, NUM_CLUSTERS, NUM_ITERATIONS)
+    sc.stop()
+
+    println("cluster-centers -> ")
+    clusters.clusterCenters.foreach(println)
   }
 }
--- src/main/scala/Constants.scala
+++ src/main/scala/Constants.scala
@@ -1,3 +1,6 @@
 object Constants {
-  val FINAL_DATA_PATH = "data/MVCFinalDataset.csv";
+  val FINAL_DATA_PATH = "data/MVCFinalDataset.csv"
+  val PARTITION_RATIO = 0.001
+  val NUM_CLUSTERS = 5
+  val NUM_ITERATIONS = 100
 }
