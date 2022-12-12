bdc59f401c6f892d6747be6004b0f42a2adb65fe
haktancetin <haktancetin@gmail.com>
2022-12-12 22:10:08 +0300

    Completed graphing system. Graph output now visible at cluster_graph.png.

--- clusterplot.py
+++ clusterplot.py
@@ -1,11 +1,25 @@
-import matplotlib
+import matplotlib.pyplot as plt
 
 vector_cluster_ids = open("data/vector-cluster-ids")
 readlines = vector_cluster_ids.readlines()
 
+fig, ax = plt.subplots()
+
+x = []
+y = []
+cluster_ids = []
+
 for line in readlines:
     line = line.replace("(", "").replace(")", "").replace("[", "").replace("]", "")
     splitline = line.split(",")
-    x_coordinate = splitline[0]
-    y_coordinate = splitline[1]
-    cluster_id = splitline[2]
+    x.append(splitline[0])
+    y.append(splitline[1])
+    cluster_ids.append(splitline[2])
+
+x = [float(i) for i in x]
+y = [float(i) for i in y]
+cluster_ids = [int(i) for i in cluster_ids]
+
+ax.scatter(x, y, c=cluster_ids, cmap='viridis', s=0.5)
+
+plt.savefig('cluster_graph.png')
